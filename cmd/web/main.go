fd48ba4b4bafdcfe06897e533d54022d08279daf
Yashin Ilya <imyashin1996@gmail.com>
2023-08-25 00:01:04 +0300

    feat: config from cli args

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type config struct {
+	host string
+	port int
+	staticDir string
+}
+
+func (cfg config) addr() string {
+	return cfg.host + ":" + fmt.Sprint(cfg.port)
+}
+
+var cfg config
+
 func main() {
+	flag.StringVar(&cfg.host, "host", "localhost", "Http network host (like 127.0.0.1 or example.org)")
+	flag.IntVar(&cfg.port, "port", 4000, "Http network port")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
+	
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(noDirFS{http.Dir("./ui/static/")})
+	fileServer := http.FileServer(noDirFS{http.Dir(cfg.staticDir)})
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view/", snippetView)
 	mux.HandleFunc("/snippet/create/", snippetCreate)
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", cfg.addr())
+	err := http.ListenAndServe(cfg.addr(), mux)
 	log.Fatalf("Err from ListenAndServe: %v", err)
 }
