d87b6004175c7c9ab941988c2f62c68e139182db
Yashin Ilya <imyashin1996@gmail.com>
2023-08-27 19:08:47 +0300

    refactor: move routes registration to separate file

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -84,9 +84,6 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 
-		// w.WriteHeader(http.StatusMethodNotAllowed)
-		// w.Write([]byte(http.StatusText(http.StatusMethodNotAllowed)))
-		// or instead
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -36,16 +36,7 @@ func main() {
 
 	app := &application{errorLog: errorLog, infoLog: infoLog}
 	
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(noDirFS{http.Dir(cfg.staticDir)})
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view/", app.snippetView)
-	mux.HandleFunc("/snippet/create/", app.snippetCreate)
-
-	server := http.Server{Addr: cfg.addr(), ErrorLog: errorLog, Handler: mux}
+	server := http.Server{Addr: cfg.addr(), ErrorLog: errorLog, Handler: app.routes()}
 
 	infoLog.Printf("Starting server on %s", cfg.addr())
 	err := server.ListenAndServe()
